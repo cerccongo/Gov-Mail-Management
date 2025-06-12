6cfd3ece5cc8ec243ca591c2f9690360a458528b
Heri Bitamala <33967676+bitamala@users.noreply.github.com>
2025-06-12 03:14:20 +0200

    Update models.py

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,10 +1,12 @@
+from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+ROLES = (
+    ('secretaire', 'Secrétariat'),
+    ('chef_cabinet', 'Chef de Cabinet'),
+    ('ministre', 'Ministre'),
+    ('admin', 'Administrateur'),
+)
+
 class Utilisateur(AbstractUser):
-    ROLES = (
-        ('secretaire', 'Secrétariat'),
-        ('chef_cabinet', 'Chef de Cabinet'),
-        ('ministre', 'Ministre'),
-        ('admin', 'Administrateur'),
-    )
     role = models.CharField(max_length=30, choices=ROLES, default='secretaire')
