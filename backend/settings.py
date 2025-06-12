c87cb9eb1c719b2ff4b8e618d4330b49d377368d
Heri Bitamala <33967676+bitamala@users.noreply.github.com>
2025-06-12 03:21:39 +0200

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = "change-me-in-production"
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["skmail.onrender.com", "localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
     "django.contrib.admin",
