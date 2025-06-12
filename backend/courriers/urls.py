3734f5e5502529b05b25f7e23f8097717741f1b6
Heri Bitamala <33967676+bitamala@users.noreply.github.com>
2025-06-12 04:19:50 +0200

    Update urls.py

--- backend/courriers/urls.py
+++ backend/courriers/urls.py
@@ -1,12 +1,17 @@
 from rest_framework.routers import DefaultRouter
-from .views import CourrierEntrantViewSet, CourrierSortantViewSet, PieceJointeViewSet
+from django.urls import path
+from .views import (
+    CourrierEntrantViewSet,
+    CourrierSortantViewSet,
+    PieceJointeViewSet,
+    public_add_courrier,
+)
 
 router = DefaultRouter()
 router.register("entrants", CourrierEntrantViewSet)
 router.register("sortants", CourrierSortantViewSet)
 router.register("piecesjointes", PieceJointeViewSet)
 
-urlpatterns = router.urls
-urlpatterns = [
+urlpatterns = router.urls + [
     path("add/", public_add_courrier, name="public_add_courrier"),
 ]
